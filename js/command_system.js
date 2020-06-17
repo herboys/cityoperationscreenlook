f146122df28f563a20a57b334cb21a1a068533cc
390533062@qq.com <[390533062@qq.com]>
2020-06-17 18:03:19 +0800

    制作指挥体系模块和头部部分

---
+++ js/command_system.js
@@ -0,0 +1,12 @@
+function addTableList(name,arr)
+{
+	$(name).find('.list-box').html('');
+	console.log($(name));
+	for(let i=0;i<arr.length;i++)
+	{
+		setTimeout(function(){
+			$(name).find('.list-box').append('<div class="table-list"><p>'+arr[i].address+'</p><p>'+arr[i].name+'</p><p>'+arr[i].name1+'</p><p class="num">'+arr[i].num+'</p><p>'+arr[i].phone+'</p></div>')
+		},200*i)
+
+	}
+}

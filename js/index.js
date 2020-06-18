f89271382c4f9de6b55a9fa3930b5a1db6eba6db
menghongyang <menghongyang@chinacloud.com.cn>
2020-06-18 08:51:14 +0800

    '专项管理'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+	getZxgl();
+})
+
 function setFont(){
 	var size=$(window).width()*0.005;
 	if(size<12)
@@ -5,4 +9,26 @@ function setFont(){
 		size=12;
 	}
 	$('html').css({'font-size':size+'px'})
+}
+
+function getZxgl(){
+	var resultInfo=[];
+	for(var i=0;i<20;i++){
+		resultInfo[i]='疫情防控'+i;
+	}
+	var tableLen =resultInfo.length;
+	if(tableLen!=20) return;
+	var str = "";
+	for (var i = 0; i < 16; i++){
+		str += '<div class="csyytz_txt text-centered">'
+			+ resultInfo[i] + '</div>'
+	}
+	$('#zxgl_detail').html(str);
+	var str1 = "";
+	for (var i = 16; i < tableLen; i++) {
+		str1 += '<div class="csyytz_txt text-centered">'
+			+ resultInfo[i] + '</div>'
+	}
+	$('#zxgl_detail1').html(str1);
+
 }

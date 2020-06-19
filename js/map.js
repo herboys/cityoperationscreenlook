e221bde93dc6c13ca364f0ad6b22719c501ab3d7
jtt <jtt@163.com>
2020-06-19 10:16:55 +0800

    撒点

--- js/map.js
+++ js/map.js
@@ -15,6 +15,7 @@ function  initMap() {
     setMapFeatures();
     addJiadingBoundary();
     addTraffic();
+    addPoiMarker();
 }
 /*设置地图风格*/
 var mapStyle = ["normal",       //0-标准
@@ -54,6 +55,7 @@ function  addJiadingBoundary() {
         geoJSON: geoJSON,
         // 还可以自定义getMarker和getPolyline
         getPolygon: function(geojson, lnglats) {
+            // var coord = convertCoordinate(lnglats);
             return new AMap.Polygon({
                 path: lnglats,
                 strokeStyle:"solid",//"dashed",
@@ -92,4 +94,67 @@ function addTraffic(){
         'interval': 600,         //刷新间隔，默认180s
     });
     map.add(traffic); //通过add方法添加图层
+}
+/*添加嘉定区主要poi点*/
+function addPoiMarker(){
+    //嘉定区政府 121.2653,31.375602
+    //南翔老街 121.308554,31.292458
+    //安亭汽车城 121.16929,31.281836
+    var p1 = {name:"嘉定区政府" , lng:121.2653 ,lat:31.375602, type:"gov"};
+    var p2 = {name:"南翔老街" , lng:121.308554 ,lat:31.292458, type:"scene"};
+    var p3 = {name:"安亭汽车城" , lng:121.16929 ,lat:31.281836, type:"car"};
+    var mapMarker = [p1,p2,p3];
+    console.log(mapMarker.length);
+    for(var i = 0 ; i < mapMarker.length ; i++){
+        console.log(JSON.stringify(mapMarker[i]));
+            // 创建一个 Icon
+        var icon = new AMap.Icon({
+            // 图标尺寸
+            size: new AMap.Size(22, 32),
+            // 图标的取图地址
+            image: './images/map/icon_'+mapMarker[i].type + '.png',
+            // 图标所用图片大小
+            imageSize: new AMap.Size(22, 32),
+            // 图标取图偏移量
+            // imageOffset: new AMap.Pixel(-9, -3)
+        });
+
+        // 将 icon 传入 marker
+        var marker = new AMap.Marker({
+            position: new AMap.LngLat(mapMarker[i].lng,mapMarker[i].lat),
+            icon: icon,
+            // offset: new AMap.Pixel(-13, -30)，
+            title: mapMarker[i].name
+        });
+        map.add(marker);
+    }
+}
+
+
+function convertCoordinate(lnglats){
+//     var gps = [116.3, 39.9];
+// AMap.convertFrom(gps, 'gps', function (status, result) {
+//   if (result.info === 'ok') {
+//     var lnglats = result.locations; // Array.<LngLat>
+//     console.log("result.locations = "+JSON.stringify(result.locations));
+//   }
+// });
+
+    // console.log("convertCoordinate lnglats = "+JSON.stringify(lnglats));
+    var coord = [];
+    for(var i = 0 ; i < lnglats.length ; i++){
+        var pts = [];
+        for(var j = 0 ; j < lnglats[i].length ; j++){
+            console.log("lnglats[i][j] = "+JSON.stringify(lnglats[i][j]));
+            AMap.convertFrom(lnglats[i][j], 'gps', function (status, result) {
+                if (result.info === 'ok') {
+                    console.log("result.locations = "+JSON.stringify(result.locations));
+                    pts.push([result.locations.lng,result.locations.lat]); // Array.<LngLat>
+                    console.log("pts = "+pts.length);
+                }
+            });
+        }
+        // coord.push(pts);
+    }
+    console.log("convertCoordinate coord = "+JSON.stringify(coord));
 }

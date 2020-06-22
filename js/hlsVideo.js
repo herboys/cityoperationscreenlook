27f71cd7d660364c8bd4550375f6302b13353405
xiaohang <xiaohang317@126.com>
2020-06-22 21:24:23 +0800

    hls

---
+++ js/hlsVideo.js
@@ -0,0 +1,16 @@
+if (Hls.isSupported()) {
+    var video = document.getElementById('video');
+    var hls = new Hls();
+    var hlssource = '';
+    $('#startbt').on('click',function () {
+        var inputurl = $('#hlsurl').val();
+        if (!inputurl==''){
+            hlssource = inputurl;
+        }
+        hls.loadSource(hlssource);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
+        })
+    });
+}

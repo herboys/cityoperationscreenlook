6e5e6a96d841dc8225968dcfe20f3538b4e834f4
xiaohang <xiaohang317@126.com>
2020-06-24 02:18:17 +0800

    样式修改

--- js/commonJs.js
+++ js/commonJs.js
@@ -1,4 +1,5 @@
 //let STATIC_URL="http://192.168.10.32:9090/citysituation";
 //let STATIC_URL="http://180.167.35.167:9090/citysituation";
 //let STATIC_URL="http://10.237.200.190:8085";
-let STATIC_URL="http://192.168.30.192:8085"
+//let STATIC_URL="http://192.168.30.192:8085"
+let STATIC_URL="http://10.237.200.190:8080/citysituation";

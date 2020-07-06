d1125426485ccdac0c6836fe7fb0c59b9f049a25
xiaohang <xiaohang317@126.com>
2020-07-06 16:27:54 +0800

    ignore

--- js/commonJs.js
+++ js/commonJs.js
@@ -2,5 +2,7 @@
 //let STATIC_URL="http://180.167.35.167:9090/citysituation";
 //let STATIC_URL="http://10.237.200.190:8085";
 //let STATIC_URL="http://192.168.30.192:8085"
-//let STATIC_URL="http://10.237.200.190:8080/citysituation";
-let STATIC_URL="http://10.237.206.140:8089/citysituation";
+
+let STATIC_URL="http://10.237.200.190:8080/citysituation";
+//let STATIC_URL="http://10.237.206.140:8089/citysituation";
+let ORACLE_URL="http://10.237.206.140:8089/oracleConnection";

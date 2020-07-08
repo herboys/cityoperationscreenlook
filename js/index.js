d7448788f593ddd968c8c20da29faa36809c1b48
xiaohang <xiaohang317@126.com>
2020-07-08 18:25:30 +0800

    两个专项管理

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+	getZxgl_old();
 	getZxgl();
 	getName();
 	setInterval(getName,12*60*60*1000)
@@ -143,6 +144,65 @@ function getZxgl(){
 		}
 	})
 }
+
+
+function getZxgl_old(){
+	//var STATIC_URL="http://localhost:8085";
+	$.ajax({
+		url : STATIC_URL+'/specialmanagement/findAll',
+		dataType : 'json',
+		type : 'get',
+		success : function(data) {
+			//alert(123)
+			//var data=res.data
+			//alert(JSON.stringify(data))
+
+			var tableLen =data.length;
+			var str = "";str1 = [];
+			for (var i = 0; i < tableLen; i++){
+
+				str += '<a  class="csyytz_txt text-centered" href= '+data[i].managementUrl+' target="_blank">'
+					+'<div class="arrowBar-box">'
+					+'<div class="arrowBar"></div>'
+					+'</div>'
+					+ data[i].managementName+ '</a>';
+				//str1[i]='<div style="width: 90%;height:50%;top: 0.1vw;left:5%">'+data[i].management_url+'</div>';
+			}
+
+
+			/*str += '<div class="csyytz_txt text-centered">'
+					+'<div class="arrowBar-box">'
+					+'<div class="arrowBar"></div>'
+					+'</div>'
+					+ '更多......</div>'*/
+			//str1[i]+='<button class="qd-btn" onclick="wblk()">关闭</button>';
+
+			$('#zxgl_detail').html(str);
+
+		},
+		error:function (data) {
+			var resultInfo=[];
+			var i=0;
+			resultInfo[i++]='城运平台';resultInfo[i++]='网格化管理';resultInfo[i++]='智慧交通';resultInfo[i++]='智慧电梯';
+			resultInfo[i++]='大客流监测';resultInfo[i++]='防台防汛';resultInfo[i++]='食药监管';resultInfo[i++]='餐厨废油';
+			resultInfo[i++]='市容车辆';resultInfo[i++]='城管执法';resultInfo[i++]='物业管理';resultInfo[i++]='社区矫正';
+			resultInfo[i++]='综治联勤';resultInfo[i++]='应急管理';resultInfo[i++]='城运通平台';resultInfo[i++]='疫情防控';
+			resultInfo[i++]='垃圾分类';resultInfo[i++]='气象预警';resultInfo[i++]='水务一体化';resultInfo[i++]='更多......';
+
+			var tableLen =resultInfo.length;
+			var str = "";
+			for (var i = 0; i < tableLen; i++){
+				str += '<div class="csyytz_txt text-centered">'
+					+'<div class="arrowBar-box">'
+					+'<div class="arrowBar"></div>'
+					+'</div>'
+					+ resultInfo[i] + '</div>'
+			}
+			$('#zxgl_detail').html(str);
+		}
+	})
+}
+
 
 function getName(){
 	var dt = getNowFormatDate();

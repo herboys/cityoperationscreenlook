1aa7514b6bbb0410f0e83116d637eb1be38b76d1
zhangnan <15605189713@163.com>
2020-08-13 11:44:55 +0800

    ignore

--- js/commonJs.js
+++ js/commonJs.js
@@ -6,7 +6,7 @@
 //let STATIC_URL="http://10.237.200.190:8080/citysituation";
 //let STATIC_URL = "http://10.237.206.140:8089/citysituation";
 //let ORACLE_URL = "http://10.237.206.140:8089/oracleConnection";
-let STATIC_URL = "http://10.237.115.83:8089/citysituation";
+// let STATIC_URL = "http://10.237.115.83:8089/citysituation";
 let ORACLE_URL = "http://10.237.115.83:8089/oracleConnection";
 //let STATIC_URL="http://10.237.200.190:8085";
 //let STATIC_URL = "http://localhost:8085";

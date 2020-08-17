22ab90a35a87cb41a19de9bbc82d6553d3685d29
zhangnan <15605189713@163.com>
2020-08-17 20:48:43 +0800

    iframe全部完成

--- js/iframe.js
+++ js/iframe.js
@@ -2,7 +2,8 @@
 function openIframe(iframeUrl){
     var str='<iframe src="'+iframeUrl+'" width="100%" height="100%" frameborder="0"></iframe>'
     $("#iframeDiv").html("")
-    $("#iframeDiv").innerHTML(str)
+    $("#iframeDiv").html(str)
+    $("#publicIframe").show()
 }
 
 function closeIframe(){

0a7b6b7d21b8b03cf3d6df082f7555356e7a5958
zhangnan <15605189713@163.com>
2020-08-18 17:33:04 +0800

    冲突解决

--- js/iframe.js
+++ js/iframe.js
@@ -1,6 +1,6 @@
 
 function openIframe(iframeUrl){
-    var str='<iframe src="'+iframeUrl+'" width="100%" height="100%" frameborder="0"></iframe>'
+    var str='<iframe src="'+iframeUrl+'" width="140%" height="140%" frameborder="0"  style="transform: scale(0.5);margin-left:-34.5%;margin-top:-10%"></iframe>'
     $("#iframeDiv").html("")
     $("#iframeDiv").html(str)
     $("#publicIframe").show()

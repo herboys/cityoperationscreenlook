bdf38c96ffd06bd3a79624c01feee19e26d9fa5b
xiaohang <xiaohang317@126.com>
2020-08-18 16:46:49 +0800

    仪表盘

--- js/rest-echarts.js
+++ js/rest-echarts.js
@@ -367,14 +367,11 @@ function initEcharts2() {
 }
 function initEcharts3(){
   var myChart3 = echarts.init(document.getElementById("ec3"));
-  var colorTemplate1 = [[0.2, "rgba(255,0,0,0.8)"], [0.8, "rgba(0,255,255,0.8)"], [1, "rgba(0,255,0,0.8)"]];
-    
-    
+  var colorTemplate1 = [[0.2, "rgba(0,255,255,0.8)"], [0.8, "rgba(0,255,255,0.8)"], [1, "rgba(0,255,255,0.8)"]];
     var data1 = [{
-                    name: "安全度",
-                    value: 85,
-                }];
-    
+          name: "20200",
+          value: 65,
+      }];
       
     // 指定图表的配置项和数据
     var option = {
@@ -411,7 +408,7 @@ function initEcharts3(){
                     lineStyle: {            // 仪表盘轴线样式。
                         color: colorTemplate1,  //仪表盘的轴线可以被分成不同颜色的多段。每段的  结束位置(范围是[0,1]) 和  颜色  可以通过一个数组来表示。默认取值：[[0.2, '#91c7ae'], [0.8, '#63869e'], [1, '#c23531']]
                         opacity: 1,                 //图形透明度。支持从 0 到 1 的数字，为 0 时不绘制该图形。
-                        width: 30,                  //轴线宽度,默认 30。
+                        width: 1,                  //轴线宽度,默认 30。
                         shadowBlur: 20,             //(发光效果)图形阴影的模糊大小。该属性配合 shadowColor,shadowOffsetX, shadowOffsetY 一起设置图形的阴影效果。 
                         shadowColor: "#fff",        //阴影颜色。支持的格式同color。
                     }
@@ -419,9 +416,9 @@ function initEcharts3(){
                 
                 splitLine: {            // 分隔线样式。
                     show: true,             // 是否显示分隔线,默认 true。
-                    length: 30,             // 分隔线线长。支持相对半径的百分比,默认 30。
+                    length: 10,             // 分隔线线长。支持相对半径的百分比,默认 30。
                     lineStyle: {            // 分隔线样式。
-                        color: "#eee",              //线的颜色,默认 #eee。
+                        color: "#00FFCC",              //线的颜色,默认 #eee。
                         opacity: 1,                 //图形透明度。支持从 0 到 1 的数字，为 0 时不绘制该图形。
                         width: 2,                   //线度,默认 2。
                         type: "solid",              //线的类型,默认 solid。 此外还有 dashed,dotted
@@ -432,10 +429,10 @@ function initEcharts3(){
                 
                 axisTick: {             // 刻度(线)样式。
                     show: true,             // 是否显示刻度(线),默认 true。
-                    splitNumber: 5,         // 分隔线之间分割的刻度数,默认 5。
-                    length: 8,              // 刻度线长。支持相对半径的百分比,默认 8。
+                    splitNumber: 3,         // 分隔线之间分割的刻度数,默认 5。
+                    length: 6,              // 刻度线长。支持相对半径的百分比,默认 8。
                     lineStyle: {            // 刻度线样式。   
-                        color: "#eee",              //线的颜色,默认 #eee。
+                        color: "#00FFCC",              //线的颜色,默认 #eee。
                         opacity: 1,                 //图形透明度。支持从 0 到 1 的数字，为 0 时不绘制该图形。
                         width: 1,                   //线度,默认 1。
                         type: "solid",              //线的类型,默认 solid。 此外还有 dashed,dotted
@@ -445,15 +442,15 @@ function initEcharts3(){
                 },
                         
                 axisLabel: {            // 刻度标签。
-                    show: true,             // 是否显示标签,默认 true。
+                    show: false,             // 是否显示标签,默认 true。
                     distance: 5,            // 标签与刻度线的距离,默认 5。
                     color: "#fff",          // 文字的颜色,默认 #fff。
-                    fontSize: 12,           // 文字的字体大小,默认 5。
+                    fontSize: 5,           // 文字的字体大小,默认 5。
                     formatter: "{value}",   // 刻度标签的内容格式器，支持字符串模板和回调函数两种形式。 示例:// 使用字符串模板，模板变量为刻度默认标签 {value},如:formatter: '{value} kg'; // 使用函数模板，函数参数分别为刻度数值,如formatter: function (value) {return value + 'km/h';}
                 },
                 
                 pointer: {              // 仪表盘指针。
-                    show: true,             // 是否显示指针,默认 true。
+                    show: false,             // 是否显示指针,默认 true。
                     length: "70%",          // 指针长度，可以是绝对数值，也可以是相对于半径的百分比,默认 80%。
                     width: 5,               // 指针宽度,默认 8。
                 },
@@ -476,16 +473,16 @@ function initEcharts3(){
                 
                 title: {                // 仪表盘标题。
                     show: true,             // 是否显示标题,默认 true。
-                    offsetCenter: [0,"20%"],//相对于仪表盘中心的偏移位置，数组第一项是水平方向的偏移，第二项是垂直方向的偏移。可以是绝对的数值，也可以是相对于仪表盘半径的百分比。
-                    color: "#fff",          // 文字的颜色,默认 #333。
-                    fontSize: 20,           // 文字的字体大小,默认 15。
+                    offsetCenter: [0,"-20%"],//相对于仪表盘中心的偏移位置，数组第一项是水平方向的偏移，第二项是垂直方向的偏移。可以是绝对的数值，也可以是相对于仪表盘半径的百分比。
+                    color: "#FFFF00",          // 文字的颜色,默认 #333。
+                    fontSize: 12,           // 文字的字体大小,默认 15。
                 },
                 
                 detail: {               // 仪表盘详情，用于显示数据。
                     show: true,             // 是否显示详情,默认 true。
-                    offsetCenter: [0,"50%"],// 相对于仪表盘中心的偏移位置，数组第一项是水平方向的偏移，第二项是垂直方向的偏移。可以是绝对的数值，也可以是相对于仪表盘半径的百分比。
+                    offsetCenter: [0,"30%"],// 相对于仪表盘中心的偏移位置，数组第一项是水平方向的偏移，第二项是垂直方向的偏移。可以是绝对的数值，也可以是相对于仪表盘半径的百分比。
                     color: "auto",          // 文字的颜色,默认 auto。
-                    fontSize: 30,           // 文字的字体大小,默认 15。
+                    fontSize: 10,           // 文字的字体大小,默认 15。
                     formatter: "{value}%",  // 格式化函数或者字符串
                 },
                 

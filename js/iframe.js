4cced7ed605b1fdc1fa3f04572ce865e831c62a5
zhangnan <15605189713@163.com>
2020-08-18 18:27:45 +0800

    iframe 滚动效果

--- js/iframe.js
+++ js/iframe.js
@@ -1,6 +1,6 @@
 
 function openIframe(iframeUrl){
-    var str='<iframe src="'+iframeUrl+'" width="140%" height="140%" frameborder="0"  style="transform: scale(0.5);margin-left:-34.5%;margin-top:-10%"></iframe>'
+    var str='<iframe src="'+iframeUrl+'" width="143%" height="143%" frameborder="0"  style="transform: scale(0.49);margin-left:-36.3%;margin-top:-10%"></iframe>'
     $("#iframeDiv").html("")
     $("#iframeDiv").html(str)
     $("#publicIframe").show()

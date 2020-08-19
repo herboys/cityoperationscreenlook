041b137714086f2db71f2b7bd731ffd3c6a21909
yueguiming <2695500678@qq.com>
2020-08-19 08:17:39 +0800

    12345模态框

--- js/MyEcharts.js
+++ js/MyEcharts.js
@@ -38,6 +38,7 @@ function isInteger(obj) {
 var MyEcharts = {
     EchartsDataFormate: {
 
+
         /**
          * @param data : json数据<br>
          * @param type : 图表类型<br>
@@ -104,7 +105,7 @@ var MyEcharts = {
        var     xData = ["本年话务总量", "本年人工话务量", "每万客户呼入量",'每万客户呼入量'];
         var    yData = [2342, 1230, 425,900];
          var   option = {
-                backgroundColor: '#061326',
+                // backgroundColor: '#061326',
                 "grid": {
                     "top": "25%",
                     "left": "-5%",
@@ -351,7 +352,9 @@ var MyEcharts = {
                 }
                 return opts;
             }
-            console.log(title,color,data,'000')
+            // console.log(title,color,data,'000')
+            //  function aaa(title){    return title.split("-").join("\n");}
+            // console.log(aaa())
             var option = {
 
                 legend: {
@@ -365,6 +368,29 @@ var MyEcharts = {
                     },
                     height:150
                 },
+                title: [{
+                    text: '{name|' + title +'}',
+
+                    top: 'center',
+                    left: 'center',
+                    textStyle: {
+                        rich: {
+                            name: {
+                                fontSize: 14,
+                                fontWeight: 'normal',
+                                color: '#fff',
+                                padding: [5, 0]
+                            },
+                            val: {
+                                fontSize: 32,
+                                fontWeight: 'bold',
+                                color: '#fff',
+                            }
+                        }
+                    }
+
+                }],
+                formatter:function(val){    return val.split("-").join("\n");},//此语句是让legend 中的文字进行换行</span>
                 animation: true,
                 series: [
                     {
@@ -445,7 +471,395 @@ var MyEcharts = {
             };
             return option;
         },
+        Ranking:function(name){
+
+            function contains(arr, dst) {
+                var i = arr.length;
+                while (i -= 1) {
+                    if (arr[i] == dst) {
+                        return i;
+                    }
+                }
+                return false;
+            }
+
+            var attackSourcesData = [70, 34, 60, 70, 34, 70, 34, 34];
+            var attackSourcesName = ['绕过攻击', '网络爬虫', '其他有害程序事件', '远程代码执行', '信息泄露', 'HTTP请求攻击', '其他注入攻击', '后门攻击'];
+            var attackSourcesColor = ['#f36c6c', '#e6cf4e', '#20d180', '#0093ff', '#1089E7', '#F57474', '#56D0E3', '#1089E7', '#F57474', '#1089E7', '#F57474', '#F57474'];
+
+            function attackSourcesDataFmt(sData) {
+                var sss = [];
+                sData.forEach(function(item, i) {
+                    itemStyle = {
+                        color: i > 3 ? attackSourcesColor[3] : attackSourcesColor[i]
+                    }
+                    sss.push({
+                        value: item,
+                        // itemStyle: itemStyle
+                    });
+                });
+                console.log(sss)
+                return sss;
+            }
+            var option = {
+                tooltip: {
+                    show: true,
+                    // backgroundColor: 'rgba(3,169,244, 0.5)',//背景颜色（此时为默认色）
+                    textStyle: {
+                        fontSize: 16
+                    },
+                    // trigger: 'axis',
+                    // axisPointer: {
+                    //    type: 'shadow'
+                    // }
+                },
+                legend: {
+                    show: false
+                },
+                grid: {
+                    left: 180,
+                    right:80
+                },
+                dataZoom: [{
+                    type: 'slider',
+                    yAxisIndex: [0,1],
+                    zoomLock: true,
+                    width: 10,
+                    handleSize: 0,
+                    showDetail: false,
+                    start: 0,
+                    right:25,
+                    end: 50,
+                    handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
+                    handleSize: '100%',
+                    handleStyle: {
+                        color: "#d3dee5",
+                    },
+                    borderColor: "#90979c"
+                }, {
+                    type: 'inside',
+                    id: 'insideY',
+                    yAxisIndex: 0,
+                    start: 0,
+                    end: 50,
+                    zoomOnMouseWheel: false,
+                    moveOnMouseMove: true,
+                    moveOnMouseWheel: true
+                }],
+                xAxis: {
+                    type: 'value',
+
+                    splitLine: {
+                        show: false
+                    },
+                    axisLabel: {
+                        show: false
+                    },
+                    axisTick: {
+                        show: false
+                    },
+                    axisLine: {
+                        show: false
+                    }
+
+                },
+                yAxis: [
+                    {
+                        type: 'category',
+                        inverse: true,
+                        splitLine: {
+                            show: false
+                        },
+                        axisTick: {
+                            show: false
+                        },
+                        axisLine: {
+                            show: false
+                        },
+                        axisPointer: {
+                            label: {
+                                show: true,
+                                margin: 30
+                            }
+                        },
+                        data: attackSourcesName,
+                        axisLabel: {
+                            margin: 140,
+                            fontSize: 14,
+                            align: 'left',
+                            color: '#333',
+                            rich: {
+                                nt1: {
+                                    color: '#fff',
+                                    backgroundColor: attackSourcesColor[0],
+                                    width: 20,
+                                    height: 20,
+                                    align: 'center',
+                                    borderRadius: 100
+                                },
+                                nt2: {
+                                    color: '#fff',
+                                    backgroundColor: attackSourcesColor[1],
+                                    width: 20,
+                                    height: 20,
+                                    align: 'center',
+                                    borderRadius: 100
+                                },
+                                nt3: {
+                                    color: '#fff',
+                                    backgroundColor: attackSourcesColor[2],
+                                    width: 20,
+                                    height: 20,
+                                    align: 'center',
+                                    borderRadius: 100
+                                },
+                                nt: {
+                                    color: '#fff',
+                                    backgroundColor: attackSourcesColor[3],
+                                    width: 20,
+                                    height: 20,
+                                    align: 'center',
+                                    borderRadius: 100
+                                },
+                                title1: {
+
+                                    color: '#fff',
+                                    width: 90,
+                                    align: 'left',
+                                    borderRadius: 5,
+                                    padding: 5,
+                                },
+                                title2: {
+
+                                    color: '#fff',
+                                    width: 90,
+                                    align: 'left',
+                                    borderRadius: 5,
+                                    padding: 5,
+                                },
+                                title3: {
+
+                                    color: '#fff',
+                                    width: 90,
+                                    align: 'left',
+                                    borderRadius: 5,
+                                    padding: 5,
+                                },
+                                title: {
+
+                                    color: '#fff',
+                                    width: 90,
+                                    align: 'left',
+                                    borderRadius: 5,
+                                    padding: 5,
+                                }
+                            },
+                            formatter: function(value, index) {
+                                index = contains(attackSourcesName, value) + 1
+                                if (index - 1 < 3) {
+                                    return [
+                                        '{nt' + index + '|' + index + '}' + '  {title' + index + '|' + value + '}'
+                                    ].join('\n')
+                                } else {
+                                    return [
+                                        '{nt|' + index + '}' + '  {title|' + value + '}'
+                                    ].join('\n')
+                                }
+                            }
+                        },
+
+                    }, {
+                        type: 'category',
+                        inverse: true,
+                        axisTick: 'none',
+                        axisLine: 'none',
+                        show: true,
+                        axisLabel: {
+                            textStyle: {
+                                color: '#ffffff',
+                                fontSize: '12'
+                            },
+                            formatter: function(value) {
+                                console.log(value,'123')
+
+                                    return (value).toLocaleString() + '%';
+
+                            },
+                        },
+                        data:attackSourcesData
+                    }],
+                series: [{
+                    z: 3,
+                    //name: 'value',
+                    type: 'bar',
+                    barWidth: '10',
+                    animationDuration: 1500,
+                    data: attackSourcesDataFmt(attackSourcesData),
+                    itemStyle: {
+                        normal: {
+                            color: function(params) {
+                                return attackSourcesColor[params.dataIndex > 3 ? 3 : params.dataIndex]
+                            },
+                            barBorderRadius: 5,
+                        }
+                    },
+                    label: {
+                        show: false,
+                        position: 'right',
+                        color: '#fff',
+                        fontSize: 14,
+                        offset: [10, 0]
+                    },
+
+                } , {
+                    name: '背景',
+                    type: 'bar',
+                    barWidth: 10,
+                    barGap: '-100%',
+                    z: 1,
+                    animationDuration: 1500,
+                    data:  [100, 100, 100, 100, 100, 100, 100, 100, 100, 1],
+                    itemStyle: {
+                        normal: {
+                            color: '#15345D',
+                            barBorderRadius: 30,
+                        }
+                    },
+
+                },
+
+                ]
+            };
+            return option;
+
+        },
+        /**
+         *@param color : 颜色 数据
+        * */
+        goods:function (color) {
+            // xdata
+            let xData = ['松山湖','南城','东城','莞城','万江','大朗','大岭山','茶山','道滘', '厚街']
+// ydata
+            let yData = [2900, 2700, 2500, 2300, 2100, 1900, 1700, 1500, 1300, 1100]
+// pictorialData
+            let pictorialData = []
+            yData.map(v => { pictorialData.push({ value: v, symbolPosition: 'end', }) })
+
+           let option = {
+               legend: {
+                   data: ['满意度', '街镇事件处理总量'],
+                  x:'center',
+                   top:30,
+                   right:0,
+                   textStyle: {
+                       fontSize: 12,
+                       color: '#ffffff'
+                   }
+               },
+               color:['#2db7f5','#FFF000'],
+
+                // tooltip: { trigger: 'axis', axisPointer: { type: 'shadow' } },
+               dataZoom: [{
+                   type: 'slider',
+                   xAxisIndex: 0,
+                   zoomLock: true,
+                   width: 400,
+                   height:10,
+                   handleSize: 0,
+                   showDetail: false,
+                   start: 0,
+                   left:"center",
+                   bottom:20,
+                   end: 50,
+                   handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
+                   handleSize: '100%',
+                   handleStyle: {
+                       color: "#d3dee5",
+                   },
+                   borderColor: "#90979c"
+               }, {
+                   type: 'inside',
+                   id: 'dataZoomX',
+                   xAxisIndex: 0,
+                   start: 0,
+                   end: 50,
+                   orient:'vertical',
+                   zoomOnMouseWheel: false,
+                   moveOnMouseMove: true,
+                   moveOnMouseWheel: true
+               }],
+                xAxis: [{
+                    type: 'category',
+                    data: xData,
+                    // axisTick: { alignWithLabel: true },
+                    axisLabel: {  textStyle: { fontSize: '90%', color: "#fff" } },
+                    // axisLine: { show: false },
+                    axisTick: false
+                },{
+                    "show" : false,
+                    data: xData,
+                }, ],
+                yAxis:[{
+                    type: 'value',
+                    name:'单位',
+                    nameTextStyle: { color: '#fff', fontSize: '90%', padding: [0, 0, 0, -45]},
+                    splitLine: { lineStyle: { color: 'rgba(255,255,255, .1)'}},
+                    axisLabel: { textStyle: { fontSize: '90%', color: "#fff" }},
+                    axisLine: { show: false },
+                    axisTick: false
+                },{
+                    type: 'value',
+                    position: 'left',
+                    nameTextStyle: {
+                        color: '#00FFFF'
+                    },
+                    splitLine: {
+                        lineStyle: {
+                            type: 'dashed',
+                            color: 'rgba(135,140,147,0.8)'
+                        }
+                    },
+                    axisLine: {
+                        show: false
+                    },
+                    axisTick: {
+                        show: false
+                    },
+                    axisLabel: {
+                        formatter: '{value}',
+                        color: '#fff',
+                        fontSize: 14
+                    }
+                },],
+                series:[{
+                    name: '满意度',
+                    type: 'bar',
+                    barWidth: 30,
+                    data: yData,
+                    itemStyle: {
+                        color: color
+                    },
+
+                },{
+                   name:'街镇事件处理总量',
+                    itemStyle: {
+                        normal: {
+                            color: "#FFF000",
+                            lineStyle: {
+                                color: "#FFF000"
+                            }
+                        }
+                    },
+                    type: "line",
+                    data: yData,
+                },
+                  ]
+            };
+            return option
+
+        }
     },
+
     /**
      *添加Id
      * @param option : option

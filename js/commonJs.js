a713474d7cfb6acaa32970d930560493ad27d746
zhangnan <15605189713@163.com>
2020-08-20 18:12:34 +0800

    iframe

--- js/commonJs.js
+++ js/commonJs.js
@@ -7,11 +7,11 @@
 //let STATIC_URL = "http://10.237.206.140:8089/citysituation";
 //let ORACLE_URL = "http://10.237.206.140:8089/oracleConnection";
 
-let STATIC_URL = "http://10.237.206.140:8089/citysituation";
-let ORACLE_URL = "http://10.237.206.140:8089/oracleConnection";
+let STATIC_URL = "http://10.237.115.83:8089/citysituation";
+let ORACLE_URL = "http://10.237.115.83:8089/oracleConnection";
 //let STATIC_URL="http://10.237.200.190:8085";
 //let STATIC_URL = "http://localhost:8085";
-let FIRE_URL = "http://10.237.206.140:8089/city-operation";
+let FIRE_URL = "http://10.237.115.83:8089/city-operation";
 //let FIRE_URL = "http://10.237.115.83:8091";
 let GarBage_URl= "http://192.168.10.12:8085";
 
--- js/iframe.js
+++ js/iframe.js
@@ -1,6 +1,6 @@
 
 function openIframe(iframeUrl){
-    var str='<iframe src="'+iframeUrl+'" width="143%" height="143%" frameborder="0"  style="transform: scale(0.49);margin-left:-36.3%;margin-top:-10%"></iframe>'
+    var str='<iframe src="'+iframeUrl+'" width="160%" height="160%" frameborder="0"  style="transform: scale(0.45);margin-left:-44%;margin-top:-12.5%"></iframe>'
     $("#iframeDiv").html("")
     $("#iframeDiv").html(str)
     $("#publicIframe").show()

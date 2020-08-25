48816c416da6f613626ab51cd6ec9449e0d0dbf0
zhangnan <15605189713@163.com>
2020-08-25 08:57:54 +0800

    video

--- js/video.js
+++ js/video.js
@@ -11,170 +11,13 @@ var videoInterval;
 $(function(){
 	createVideoSlide();
 	createVideoSlideOne();
-	openLargeVideo();
+
+	//openLargeVideo('G15嘉定朱桥一级检查站1','http://10.237.221.178:8050/cam/realmonitor/31011401001320221006?subtype=0&streamType=0&token=1598235258_411db1feac35a0fa51686c37e661360923c421e2&mediatype=HLS.m3u8')
 	videoInterval=setInterval(removeVideoCache,15*60*1000) //每15分钟清空一次缓存
 })
 
-function getjrwlzsj(){
-	$.ajax({// 根据指标获取相应的数据 findDetailByCode
-		url : '../../../sh/SocialAutonomySystem/todayUntrans',
-		dataType : 'json',
-		type : 'post',
-		contentType : 'application/json',
-		data : JSON.stringify({}),
-		async : true,
-		success : function(data) {
-			if (data.result == 'OK') {
-				for(var i=0;i<10;i++){
-					jrwlzsjList[i]='http://vali-g1.cp31.ott.cibntv.net/youku/69752e30609387173282e501c/03000801005ED0E635F6204003E880EAF120E3-FC36-4F82-A448-5E34401D7A65.mp4?sid=159212567300010004125_00_B54712ec8006757e140762bf6c08f5dd7&amp;sign=beefe9d075ef0e2d21a32f31bedc26f6&amp;ctype=50&amp;si=183&amp;psid=713850910b6824a787ce59f500f54be24d045'
-				}
-
-				var str = "";
-				var tableLen = data.resultInfo.length;
-				jrwlzsjList=data.resultInfo
-				for (var i = 0; i < 3; i++) {
-                    str += '<div class="slide">'
-							+'<div class="jrwlzsjTxt">'
-				
-					str +='<div class="jrwlzsjCont">'
-
-					if(i==0||i==1||i==2)
-					{
-						str	+='<video controls="" autoplay preload muted name="media" class="video-list"  src="'+jrwlzsjList[i]+'">'
-					}
-					
-					str	+= '  </video>'
-						+'</div>'
-						+'<div class="sjxqTitle sjtit2">'+"立交防汛点位"+i+'</div>'
-						+'</div>'
-						+'</div>'
-				}
-				$('#slider2').html(str);
-				if(i >2 ){
-					$(document).ready(function() {
-						intVideo();
-						$('.jrwlzsj_box').mouseenter(function(){
-							clearInterval(videoTime)
-						})
-						$('.jrwlzsj_box').mouseleave(function(){
-							intVideo();
-						})	
-					});
-				}
-			} else {
-				$('#cont1').html('查询失败');
-			}
-		}
-	})
-}
 
 var videoFlagTwo=0;
-/*function createVideoSlide(){
-
-	var G215List=[]
-	var G215Name=[]
-	var index=0;
-	$.ajax({
-		url:STATIC_URL+'/videoStream/getVideoSource/2',
-		type:'get',
-		dataType: "json",
-		async:false,
-		success:function (data) {
-			var resData=data.data;
-			console.log(resData)
-
-			for(var i=0;i<resData.length;i++){
-				if(resData[i].url!=null && resData[i].url!=''){
-					//alert(resData[i].url)
-					G215List[index]=resData[i].url
-					G215Name[index++]=resData[i].monitorPoint.substr(9)
-				}
-			}
-		},
-		error:function (data) {
-			console.log(data)
-			alert('失败')
-		}
-
-	})
-
-	$.ajax({
-		url:STATIC_URL+'/videoStream/getVideoSource/1',
-		type:'get',
-		dataType: "json",
-		async:false,
-		success:function (data) {
-			var resData=data.data;
-
-			for(var i=0;i<resData.length;i++){
-				if(resData[i].url!=null && resData[i].url!=''){
-					G215List[index]=resData[i].url
-					G215Name[index++]=resData[i].monitorPoint.substr(9)
-				}
-			}
-		},
-		error:function (data) {
-			console.log(data)
-			alert('失败')
-		}
-
-	})
-
-
-	console.log(123)
-	console.log(G215List)
-
-    for(var i=0;i<G215List.length;i++){
-        var videoObj=new Object();
-        videoObj.name=G215Name[i]
-        videoObj.src=G215List[i]
-        jrwlzsjList[i]=videoObj;
-    }
-
-    var str=""
-	$('#slider2').html('');
-    for (var i = 0; i < 3; i++) {
-    	str=''
-        str += '<div class="slide">'
-                +'<div class="jrwlzsjTxt"><span style="left:0px;top:-5px;font-size:0.95rem;position:absolute;color:#00fff6;z-index:999">'+jrwlzsjList[i].name+'</span>'
-
-        str +='<div class="jrwlzsjCont">'
-
-		// str	+='<video controls="" autoplay preload muted name="media" class="video-list" src="'+jrwlzsjList[i].src+'">'
-		str	+='<video controls="" autoplay preload muted name="media" class="video-list" muted="muted">'
-
-        str	+= '  </video>'
-            +'</div>'
-            +'</div>'
-            +'</div>'
-
-		$('#slider2').append(str);
-
-		var hls = new Hls();
-		var video = $("#slider2 video")[i];
-		hls.loadSource(jrwlzsjList[i].src);
-		hls.attachMedia(video);
-		hls.on(Hls.Events.MANIFEST_PARSED, function () {
-			video.play();
-		})
-
-    }
-    //$('#slider2').html(str);
-    if(i >2 ){
-        $(document).ready(function() {
-            intVideoTwo();
-            $('.jrwlzsj_box').mouseenter(function(){
-                clearInterval(videoTime)
-            })
-            $('.jrwlzsj_box').mouseleave(function(){
-				if(videoFlagTwo==0)
-                	intVideoTwo();
-            })	
-        });
-    }
-
-}*/
-
 
 function createVideoSlide(){
 
@@ -202,18 +45,24 @@ function createVideoSlide(){
 
 			for(var i=0;i<G215List.length;i++){
 				var videoObj=new Object();
-				videoObj.name=G215Name[i]
+				videoObj.name=G215Name[i].replace(" ","_")
 				videoObj.src=G215List[i]
 				jrwlzsjList[i]=videoObj;
 			}
+
 
 			var str=""
 			$('#slider2').html('');
 			for (var i = 0; i < 1; i++) {
 				str=''
+				var name=jrwlzsjList[i].name
+				name=name.replace(" ","_");
+				var url=jrwlzsjList[i].src
+				//openLargeVideo(name,url)
 				str += '<div class="slide1">'
 					+'<div class="jrwlzsjTxt1"><span style="left:0.5rem;top:0px;font-size:1.1rem;position:absolute;color:#00fff6;z-index:999">'+jrwlzsjList[i].name+'</span>'
-					+'<img onclick="openLargeVideo(jrwlzsjList[i].name,jrwlzsjList[i].src)" src="images/fullscreen.png"/>'
+				//	+'<img class="fullscreenImg" onclick="openLargeVideo("'+jrwlzsjList[i].name+'","'+jrwlzsjList[i].src+'")" src="images/fullscreen.png"/>'
+					+ "<img class='fullscreenImg' style='right:1rem' onclick=openLargeVideo('"+name+"','"+url+"') src='images/fullscreen1.png'/>"
 
 				str +='<div class="jrwlzsjCont1">'
 
@@ -308,41 +157,24 @@ function createVideoSlideOne(){
 
 			for(var i=0;i<G215List.length;i++){
 				var videoObj=new Object();
-				videoObj.name=G215Name[i]
+				videoObj.name=G215Name[i].replace(" ","_")
 				videoObj.src= G215List[i]
 				jrwlzsjListOne[i]=videoObj;
 			}
 
-			/*var str=""
-			$('#slider1').html('');
-			str += '<div class="slide1">'
-				+'<div class="jrwlzsjTxt1"><span style="left:0.5rem;top:0px;font-size:1.1rem;position:absolute;color:#00fff6;z-index:999">'+jrwlzsjListOne[0].name+'</span>'
-
-			str +='<div class="jrwlzsjCont1">'
-
-			str	+='<video controls="" autoplay preload muted name="media" style="width:29.2rem;height:16rem" muted="muted">'
-
-			str	+= '  </video>'
-				+'</div>'
-				+'</div>'
-				+'</div>'
-			$('#slider1').html(str);
-			var hls = new Hls();
-			var video = $("#slider1 video")[0];
-			//alert(jrwlzsjListOne[1].src)
-			hls.loadSource(jrwlzsjListOne[0].src);
-			hls.attachMedia(video);
-			hls.on(Hls.Events.MANIFEST_PARSED, function () {
-				video.play();
-			})*/
 
 			var str=""
 			$('#slider1').html('');
 			for (var i = 0; i < 1; i++) {
+				var name=jrwlzsjListOne[i].name
+				name=name.replace("","_");
+				var url=jrwlzsjListOne[i].src
 				str=''
 				str += '<div class="slide1">'
 					+ '<div class="jrwlzsjTxt1"><span style="left:0.5rem;top:0px;font-size:1.1rem;position:absolute;color:#00fff6;z-index:999">'+jrwlzsjListOne[i].name+'</span>'
-					+ '<img onclick="openLargeVideo(jrwlzsjListOne[i].name,jrwlzsjListOne[i].src)" src="images/fullscreen.png"/>'
+					//+ '<img class="fullscreenImg" onclick="openLargeVideo("'+jrwlzsjListOne[i].name+'","'+jrwlzsjListOne[i].src+'")" src="images/fullscreen.png"/>'
+					/*+ '<img class="fullscreenImg" onclick="openLargeVideo("'+name+'","'+url+'")" src="images/fullscreen.png"/>'*/
+					+ "<img class='fullscreenImg' style='right:1rem' onclick=openLargeVideo('"+name+"','"+url+"') src='images/fullscreen1.png'/>"
 				str +='<div class="jrwlzsjCont1">'
 
 				str	+='<video controls="" autoplay preload muted name="media" style="width:29.2rem;height:16rem" muted="muted">'
@@ -451,9 +283,11 @@ function insertOneVideo(){
 	console.log($('#slider1').children().eq(0));
 	var str=""
 	$('#slider1').html('');
+	var name=videoName[0]
+	var url=videoList[0]
 	str += '<div class="slide1">'
 		+'<div class="jrwlzsjTxt1"><span style="left:0.5rem;top:0px;font-size:1.1rem;position:absolute;color:#00fff6;z-index:999">'+videoName[0]+'</span>'
-		+'<img onclick="openLargeVideo(videoName[0],videoList[0])" src="images/fullscreen.png"/>'
+		+ "<img class='fullscreenImg' onclick=openLargeVideo('"+name+"','"+url+"') src='images/fullscreen1.png'/>"
 		+'<span style="right:0px;top:0px;font-size:1.1rem;position:absolute;color:#999;z-index:999" onclick=closeVideoOne()>关闭</span>'
 
 	str +='<div class="jrwlzsjCont1">'
@@ -491,10 +325,12 @@ function insertSecondVideo(){
 	delete hlsListMap["slideTwo"]
 
 	$('#slider2').html('');
+	var name=videoName[1]
+	var url=videoList[1]
 	var str=''
 	str += '<div class="slide1">'
 		+'<div class="jrwlzsjTxt1"><span style="left:0.5rem;top:0px;font-size:1.1rem;position:absolute;color:#00fff6;z-index:999">'+videoName[1]+'</span>'
-		+'<img onclick="openLargeVideo(videoName[1],videoList[1])" src="images/fullscreen.png"/>'
+		+ "<img class='fullscreenImg' onclick=openLargeVideo('"+name+"','"+url+"') src='images/fullscreen1.png'/>"
 		+'<span style="right:0px;top:0px;font-size:1.1rem;position:absolute;color:#999;z-index:999" onclick=closeVideoTwo()>关闭</span>'
 	str +='<div class="jrwlzsjCont1">'
 
@@ -546,10 +382,13 @@ function insertTwoVideo(first,second){
 		video.play();
 	})
 
+	var name=videoName[second]
+	var url=videoList[second]
+
 		str=''
 		str += '<div class="slide">'
 				+'<div class="jrwlzsjTxt"> <span style="left:0px;top:-5px;font-size:0.95rem;position:absolute;color:#00fff6;z-index:999">'+videoName[second]+'</span>' +
-				+ '<img onclick="openLargeVideo(videoName[second],videoList[second])" src="images/fullscreen.png"/>'
+			+ "<img class='fullscreenImg' onclick=openLargeVideo('"+name+"','"+url+"') src='images/fullscreen1.png'/>"
 				+ '<span style="right:0px;top:0px;font-size:0.98rem;position:absolute;color:#999;z-index:999" onclick=closeVideoTwo(1)>关闭</span>'
 		
 		str +='<div class="jrwlzsjCont">'
@@ -715,11 +554,13 @@ function removeVideoCache(){
 var videoFlagThree=0;
 function openLargeVideo(name,videoUrl){
 
-	str= '<span style="left:0px;top:0px;font-size:1.5rem;position:absolute;color:#00fff6;z-index:10000">'+name+'</span>'
+	var str= '<span style="left:0px;top:0px;font-size:1.5rem;position:absolute;color:#00fff6;z-index:10000">'+name+'</span>'
 	+ '<span style="right:0px;top:0px;font-size:1.5rem;position:absolute;color:#999;z-index:10000" onclick="removeLargeVideo()">关闭</span>'
 	+ '<div class="largeVideoDiv">'
 	+ '<video controls="" autoplay preload muted name="media" class="largeVideo"  muted="muted"></video>'
 	+ '</div>'
+	$("#slider3").html(str)
+
 	var hls = new Hls();
 	var video = $("#slider3 video")[0];
 	hls.loadSource(videoUrl);
@@ -734,7 +575,6 @@ function openLargeVideo(name,videoUrl){
 }
 
 function removeLargeVideo(){
-	alert(123)
 	var removeVideo= $("#slider3 video")[0]
 	removeVideo.pause()
 //	hlsListMap["slideThree"].destroy();

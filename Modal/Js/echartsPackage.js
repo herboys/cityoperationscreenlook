8b9cf501a8a24ac602e2a03edff9c00fb8fec76a
yuanziqi <zqcool227@163.com>
2020-08-31 15:43:27 +0800

    网格化模块优化

--- Modal/Js/echartsPackage.js
+++ Modal/Js/echartsPackage.js
@@ -658,226 +658,107 @@ var MyEcharts2 = {
             return option
         },
         PieChart: function (name, pieData) {
-            var data = pieData
-            arrName = getArrayValue(data, "name");
-            arrValue = getArrayValue(data, "value");
-            sumValue = 1000;
-            objData = array2obj(data, "name");
-            optionData = getData(data)
-
-            function getArrayValue(array, key) {
-                var key = key || "value";
-                var res = [];
-                if (array) {
-                    array.forEach(function (t) {
-                        res.push(t[key]);
-                    });
-                }
-                return res;
-            }
-
-            function array2obj(array, key) {
-                var resObj = {};
-                for (var i = 0; i < array.length; i++) {
-                    resObj[array[i][key]] = array[i];
-                }
-                return resObj;
-            }
-
-            function getData(data) {
-                var res = {
-                    series: [{
-                            name: "大环",
-                            type: 'gauge',
-                            splitNumber: 15,
-                            radius: '82%',
-                            center: ['50%', '55%'],
-                            startAngle: 90,
-                            endAngle: -269.9999,
-                            axisLine: {
-                                show: false,
-                                lineStyle: {
-                                    color: [
-                                        [1, '#1f59a7']
-                                    ]
-                                }
-                            },
-                            axisTick: {
-                                show: false
-                            },
-                            splitLine: {
-                                show: true,
-                                length: 32,
-                                lineStyle: {
-                                    color: 'auto',
-                                    width: 3.5
-                                }
-                            },
-                            axisLabel: {
-                                show: false
-                            },
-                            detail: {
-                                show: false
-                            }
-                        },
-                        {
-                            name: '小环',
-                            type: 'gauge',
-                            splitNumber: 15,
-                            radius: '78%',
-                            center: ['50%', '55%'],
-                            startAngle: 90,
-                            endAngle: -269.9999,
-                            axisLine: {
-                                show: false
-                            },
-                            axisTick: {
-                                show: true,
-                                lineStyle: {
-                                    color: '#1f59a7',
-                                    width: 3
-                                },
-                                length: 20,
-                                splitNumber: 5
-                            },
-                            splitLine: {
-                                show: false
-                            },
-                            axisLabel: {
-                                show: false
-                            },
-                            detail: {
-                                show: false
-                            }
-                        },
-                    ],
-                    yAxis: []
-                };
-                for (let i = 0; i < data.length; i++) {
-                    res.series.push({
-                        name: '学历',
-                        type: 'pie',
-                        clockWise: true,
-                        z: 2,
-                        hoverAnimation: false,
-                        radius: [73 - i * 15 + '%', 68 - i * 15 + '%'],
-                        center: ["50%", "55%"],
-                        label: {
-                            show: true,
-                            formatter: '{d}%',
-                            color: 'RGB(246,175,101)',
-                            fontSize: 25,
-                            position: 'inside'
-                        },
-                        labelLine: {
-                            show: false
-                        },
-                        data: [{
-                            value: data[i].value,
-                            name: data[i].name
-                        }, {
-                            value: sumValue - data[i].value,
-                            name: '',
-                            itemStyle: {
-                                color: "rgba(0,0,0,0)",
-                                borderWidth: 0
-                            },
-                            tooltip: {
-                                show: false
-                            },
-                            label: {
-                                show: false
-                            },
-                            hoverAnimation: false
-                        }]
-                    });
-                    res.series.push({
-                        name: '背景线',
-                        type: 'pie',
-                        silent: true,
-                        z: 1,
-                        clockWise: true,
-                        hoverAnimation: false,
-                        radius: [71 - i * 15 + '%', 69 - i * 15 + '%'],
-                        center: ["50%", "55%"],
-                        label: {
-                            show: false
-                        },
-                        itemStyle: {
-                            label: {
-                                show: false,
-                            },
-                            labelLine: {
-                                show: false
-                            },
-                            borderWidth: 5,
-                        },
-                        data: [{
-                            value: 100,
-                            itemStyle: {
-                                color: "RGB(12,64,128)",
-                                borderWidth: 0
-                            },
-                            tooltip: {
-                                show: false
-                            },
-                            hoverAnimation: false
-                        }]
-                    });
-                    res.yAxis.push(data[i].name);
-                }
-                return res;
-            }
-
-            option = {
-                backgroundColor: 'RGB(8,20,67)',
-                color: [{
-                    type: 'linear',
-                    x: 0,
-                    y: 0,
-                    x2: 1,
-                    y2: 1,
-                    colorStops: [{
-                        offset: 0,
-                        color: 'rgba(10,31,95,1)'
-                    }, {
-                        offset: 1,
-                        color: 'rgba(1,232,254,1)'
-                    }],
-                    global: false
-                }],
-                grid: {
-                    top: '16%',
-                    bottom: '54%',
-                    left: "50%",
-                    containLabel: false
+            var placeHolderStyle = {
+                normal: {
+                    color: 'rgba(124,228,245,0.2)',
                 },
-                yAxis: [{
-                    type: 'category',
-                    inverse: true,
-                    z: 3,
-                    axisLine: {
-                        show: false
-                    },
-                    axisTick: {
-                        show: false
-                    },
-                    axisLabel: {
-                        interval: 0,
-                        inside: false,
-                        textStyle: {
-                            color: "RGB(78,184,252)",
-                            fontSize: 25,
-                        },
-                        show: true
-                    },
-                    data: optionData.yAxis
-                }],
-                xAxis: [{
-                    show: false
-                }],
-                series: optionData.series
             };
+            option = {
+                // backgroundColor:'rgba(2, 120, 200, 1)',
+                title: {
+                    text: '',
+                    textStyle: {
+                        fontWeight: 'bold',
+                        fontSize: 14,
+                        color: '#0ab7ff'
+                    },
+                    left: '20%',
+                    top: '25'
+                },
+                tooltip: {
+                    show: true,
+                    formatter: "{a}：{d}%"
+                },
+                legend: {
+                    orient: '',
+                    itemGap: 12,
+                    left: '5%',
+                    top: 'center',
+                    textStyle: {
+                        color: "#fff",
+                    },
+                    data: ['', '', '']
+                },
+            
+                series: [{
+                        name: '销量2',
+                        type: 'pie',
+                        clockWise: true,
+                        hoverAnimation: false, //鼠标移入变大
+                        radius: ['50%', '60%'],
+                        center:['35%', '50%'],
+                        itemStyle: {
+                            normal: {
+                                color: 'rgb(32, 209, 128)',
+                                label: {
+                                    show: false
+                                },
+                                labelLine: {
+                                    show: false
+                                }
+                            }
+                        },
+            
+                        data: [{
+                                value: 60,
+                                name: 'invisible',
+                                itemStyle: placeHolderStyle
+                            },
+                            {
+                                value: 40,
+                                name: '销量2'
+                            }
+                        ]
+                    },
+                    {
+                        name: '销量1',
+                        type: 'pie',
+                        clockWise: true,
+                        hoverAnimation: false, //鼠标移入变大
+                        radius: ['35%', '45%'],
+                        center:['35%', '50%'],
+                        itemStyle: {
+                            normal: {
+                                color: 'rgb(0, 147, 255)',
+                                label: {
+                                    show: false
+                                },
+                                labelLine: {
+                                    show: false
+                                }
+                            }
+                        },
+            
+                        data: [{
+                                value: 70,
+                                name: 'invisible',
+                                itemStyle: placeHolderStyle
+                            },
+                            {
+                                value: 10,
+                                name: '销量1'
+                            }
+                        ]
+                    }
+            
+                ]
+            };
+
+        
+
+           
+
+           
             return option
         }
     },

791a2d6bf7d4f1408af2996b8e3e578eab04eca2
xiaohang <xiaohang317@126.com>
2020-09-10 15:05:54 +0800

    垃圾分类

--- Modal/Js/onload.js
+++ Modal/Js/onload.js
@@ -1,12 +1,183 @@
 function newonload() {
-    getPopupFun1()
+    getGarbageCarLoadingName()
     GetCarNewInfoFun()
     initMap();
     init1()
     EndFacilities()
     TownLifeGarbage()
-    getGarbageCarLoading()
     intervalFunction()
+    OperaTionBtn(4)
+    rollInit()
+    rollInit1()
+}
+/**轮播动画**/
+function rollInit() {
+    let ul1 = document.getElementById("GongDanID");
+    let ul2 = document.getElementById("GongDanIDCopy");
+    let rollbox = document.getElementById("GongDanIDBox");
+    roll(50, ul1, ul2, rollbox)
+}
+
+function roll(t, ul1, ul2, rollbox) {
+    ul2.innerHTML = ul1.innerHTML;
+    rollbox.scrollTop = 0;
+    let lis = document.querySelectorAll("#GongDanID ul")
+    for (let i = 0; i < lis.length; i++) {
+        lis[i].onclick = function () {
+            let childLI = document.querySelectorAll(".work-older-list-ul .ul-line")
+            let para = `
+              <p style="font-size: 1.5rem;color: white;padding-left1.667rem:">诉求内容</p>
+                                            <div style="padding: 1.667rem 3.333rem;color: #d0c7c7;line-height:2rem;height: 19.333rem ">${gongdanlist[i].DESCRIPTION}</div>
+            `
+            document.getElementById("ModalsmallID").style.display = "block"
+            document.getElementById("ModalsmallRoomID").innerHTML = para
+        }
+    }
+    let liscopy = document.querySelectorAll("#GongDanIDCopy ul")
+    for (let i = 0; i < liscopy.length; i++) {
+        liscopy[i].onclick = function () {
+            let para = `
+       <p style="font-size: 1.5rem;color: white;padding-left1.667rem:">诉求内容</p>
+                          <div style="padding: 1.667rem 3.333rem;
+                          color: #d0c7c7;line-height:2rem;height: 19.333rem ">${gongdanlist[i].DESCRIPTION}</div>
+            `
+            document.getElementById("ModalsmallID").style.display = "block"
+            document.getElementById("ModalsmallRoomID").innerHTML = para
+        }
+    }
+    let timer = setInterval(rollStart, t);
+    rollbox.onmouseover = function () {
+        clearInterval(timer);
+    }
+    rollbox.onmouseout = function () {
+        timer = setInterval(rollStart, t);
+    }
+}
+
+function rollStart() {
+    let ul1 = document.getElementById("GongDanID");
+    let rollbox = document.getElementById("GongDanIDBox");
+    if (rollbox.scrollTop >= ul1.scrollHeight) {
+        rollbox.scrollTop = 0;
+    } else {
+        rollbox.scrollTop++;
+    }
+}
+
+let para = ''
+
+function OperaTionBtn(t) {
+    switch (t) {
+        case 1:
+            para = ''
+            para = `      <div class="banner-span" style="color: white" onclick="OperaTionBtn(1)">
+                                            当日投入运营车辆数
+                                        </div>
+                                        <div class="banner-span" onclick="OperaTionBtn(2)">
+                                            分类运输车辆在线监测
+                                        </div>
+                                        <div class="banner-span " onclick="OperaTionBtn(3)">
+                                            异常装载车辆在线监测${BCName}
+                                        </div>`
+            document.getElementById("CarId").innerHTML = para
+            document.getElementById("getPopup1IDcopy").innerHTML = ''
+
+            getPopupFun1()
+
+            break;
+        case 2:
+            para = ''
+            para = `      <div class="banner-span"  onclick="OperaTionBtn(1)">
+                                            当日投入运营车辆数
+                                        </div>
+                                        <div class="banner-span"style="color: white" onclick="OperaTionBtn(2)">
+                                            分类运输车辆在线监测
+                                        </div>
+                                        <div class="banner-span " onclick="OperaTionBtn(3)">
+                                            异常装载车辆在线监测${BCName}
+                                        </div>`
+            document.getElementById("CarId").innerHTML = para
+            document.getElementById("getPopup1IDcopy").innerHTML = ''
+            getGarbageCarLoading()
+            break;
+        case 3:
+            para = ''
+            para = `      <div class="banner-span"  onclick="OperaTionBtn(1)">
+                                            当日投入运营车辆数
+                                        </div>
+                                        <div class="banner-span" onclick="OperaTionBtn(2)">
+                                            分类运输车辆在线监测
+                                        </div>
+                                        <div class="banner-span "  style="color: white"onclick="OperaTionBtn(3)">
+                                            异常装载车辆在线监测${BCName}
+                                        </div>`
+            document.getElementById("CarId").innerHTML = para
+            getGarbageCarLoadingtwo()
+            break;
+        case 4:
+            para = ''
+            para=`   <div class="banner-span" style="margin-right: 2rem;color: white" onclick="OperaTionBtn(4)">
+                                            主动发现
+                                        </div>
+                                        <div class="banner-span" onclick="OperaTionBtn(5)">
+                                            被动发现
+                                        </div>`
+            document.getElementById('FindId').innerHTML=para
+            break;
+        case 5:
+            para = ''
+            para=`   <div class="banner-span" style="margin-right: 2rem;" onclick="OperaTionBtn(4)">
+                                            主动发现
+                                        </div>
+                                        <div class="banner-span"style="color: white" onclick="OperaTionBtn(5)">
+                                            被动发现
+                                        </div>`
+            document.getElementById('FindId').innerHTML=para
+            FindAfter()
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+    }
+}
+
+/*@name:被动发现*/
+function FindAfter(){
+    let para = {
+        url: ORACLE_URL + '/taskInfo/findrefuse',
+        async: true,
+        type: 'post',
+        data: JSON.stringify({
+            "date": "年"
+        }),
+        dataType: 'JSON',
+    }
+    ajaxPromise(para).then(res=>{
+     console.log(res)
+        para = `  <ul class="work-older-list-ul" >
+                                        <li>工单编号</li>
+                                        <li>发生时间</li>
+                                        <li>主责部门</li>
+                                        <li>截至时间</li>
+                                        <li>管理要点</li>
+                                        <li>诉求内容</li>
+                                    </ul>`
+        document.getElementById("GongDanTitleID").innerHTML = para
+        para=''
+        for (let i = 0; i < res.length; i++) {
+            para += '<ul class="work-older-list-ul ul-line ">'
+                + '<li>' + res[i].attributes.TASKID + '</li>'
+                + '<li>' + res[i].attributes.DISCOVERTIME + '</li>'
+                + '<li>' + res[i].attributes.EXECUTEDEPTNAME + '</li>'
+                + '<li>' + res[i].attributes.LASTSOLVINGTIME + '</li>'
+                + '<li>' + res[i].attributes.ATNAME + '</li>'
+                + '<li>' + res[i].attributes.DESCRIPTION + '</li>'
+                + '</ul>'
+        }
+        document.getElementById("GongDanID").innerHTML = para
+    })
+
 
 }
 
@@ -79,9 +250,9 @@ function TownLifeGarbage() {
             document.getElementById("dryGarbageID").innerHTML = para
             para = `<p>湿垃圾产生量</p><p >${recoverable1}吨/日</p>`
             document.getElementById("recoverableID").innerHTML = para
-            res.data.map(item=>{
-                if (item.recoverable.length==0){
-                    item.recoverable="0吨/日"
+            res.data.map(item => {
+                if (item.recoverable.length == 0) {
+                    item.recoverable = "0吨/日"
                 }
             })
 
@@ -138,7 +309,7 @@ function GetCarNewInfoFun() {
     })
     getGarbageRectify()
 }
-
+/*@name:当日投入车辆数*/
 function getPopupFun1() {
     let para = {
         url: GarBage_URl + '/sh/garbageSort/getPopup/1',
@@ -147,7 +318,6 @@ function getPopupFun1() {
         dataType: 'JSON',
     }
     ajaxPromise(para).then(res => {
-        console.log(res, '11111 ')
         let e = ''
         e = res.data[res.data.length - 1]
         if (res.status == "OK") {
@@ -191,7 +361,25 @@ function getGarbageRectify() {
     })
 
 }
-
+let BCName=''
+/*@name:车辆*/
+function getGarbageCarLoadingName() {
+     BCName=''
+        let para = {
+            url: GarBage_URl + '/sh/garbageSort/getGarbageCarLoading',
+            async: true,
+            type: 'get',
+            dataType: 'JSON',
+        }
+        ajaxPromise(para).then(res => {
+            para = ''
+            para = `<span style="color:#e7cb35">${res.data[0].garbageTranVels[res.data[0].garbageTranVels.length - 1].sum}${res.data[0].garbageTranVels[res.data[0].garbageTranVels.length - 1].unit}</span>`
+            BCName=para
+            document.getElementById("BCName").innerHTML = para
+            OperaTionBtn(1)
+        })
+}
+/*@name:分类运输车辆在线监测*/
 /**
  * 当日投入运营车辆数
  * */
@@ -204,21 +392,17 @@ function getGarbageCarLoading() {
             dataType: 'JSON',
         }
         ajaxPromise(para).then(res => {
-            console.log(res, '11111 ')
-
             if (res.status == 'OK') {
                 para = ''
                 for (let i = 0; i < res.data[0].garbageTranVels.length - 1; i++) {
-
-                    para += ' <div class="topicItem"  >'
-                        + '<p>' + res.data[0].garbageTranVels[i].popIndex.slice(2, res.data[0].garbageTranVels[i].popIndex.length) + '</p>'
+                    para += ' <div class="topicItem">'
+                        + '<p>' + res.data[0].garbageTranVels[i].popIndex.slice(2, res.data[0].garbageTranVels[i].popIndex.length)+'</p>'
                         + ' <span>' + res.data[0].garbageTranVels[i].sum + res.data[0].garbageTranVels[i].unit + '</span>'
                         + ' </div>'
                 }
-                console.log(res.data[0].garbageTranVels[res.data[0].garbageTranVels.length - 1], '0999999999')
-                document.getElementById("getGarbageCarLoadingID").innerHTML = para
+                document.getElementById("getPopup1ID").innerHTML = para
                 para = `<p>${res.data[0].garbageTranVels[res.data[0].garbageTranVels.length - 1].popIndex}</p>
-                                            <span>${res.data[0].garbageTranVels[res.data[0].garbageTranVels.length - 1].sum}${res.data[0].garbageTranVels[res.data[0].garbageTranVels.length - 1].unit}</span>`
+                        <span>${res.data[0].garbageTranVels[res.data[0].garbageTranVels.length - 1].sum}${res.data[0].garbageTranVels[res.data[0].garbageTranVels.length - 1].unit}</span>`
                 document.getElementById("lasttopicItemID").innerHTML = para
                 para = ''
                 res.data[0].garbageCarLoading.forEach(e => {
@@ -245,8 +429,8 @@ function getGarbageCarLoading() {
                 })
                 document.getElementById("OnDutyToday1").innerHTML = para
                 ul1 = document.getElementById("OnDutyToday1");
-                 ul2 = document.getElementById("OnDutyTodayCopy1");
-                 rollbox = document.getElementById("OnDutyToday_box1");
+                ul2 = document.getElementById("OnDutyTodayCopy1");
+                rollbox = document.getElementById("OnDutyToday_box1");
                 rolls1(50, ul1, ul2, rollbox)
 
 
@@ -256,6 +440,59 @@ function getGarbageCarLoading() {
     })
 
 }
+/*@name:异常装载车辆在线监测*/
+function getGarbageCarLoadingtwo(){
+    let para = {
+        url: GarBage_URl + '/sh/garbageSort/getGarbageCarLoading',
+        async: true,
+        type: 'get',
+        dataType: 'JSON',
+    }
+    ajaxPromise(para).then(res => {
+        para=''
+        res.data[0].garbageCarLoading.forEach(e => {
+            para += `<div class="topicItem topicItemBtn" >
+                                       <span class="font-10">${e.carNumber}</span>
+                                          <span class="font-10">${e.loadingRate}</span>
+                                      </div>`
+        })
+        document.getElementById("getPopup1ID").innerHTML = para
+
+    })
+}
+/**轮播动画**/
+function rollInit1() {
+    let ul1 = document.getElementById("getPopup1ID");
+    let ul2 = document.getElementById("getPopup1IDcopy");
+    let rollbox = document.getElementById("getPopup1ID-box");
+    timer=null
+    roll1(50, ul1, ul2, rollbox)
+}
+
+function roll1(t, ul1, ul2, rollbox) {
+
+    ul2.innerHTML = ul1.innerHTML;
+    rollbox.scrollTop = 0;
+    let timer = setInterval(rollStart1, t);
+    rollbox.onmouseover = function () {
+        clearInterval(timer);
+    }
+    rollbox.onmouseout = function () {
+        timer = setInterval(rollStart1, t);
+    }
+}
+
+function rollStart1() {
+    let ul1 = document.getElementById("getPopup1ID");
+    let rollbox = document.getElementById("getPopup1ID-box");
+    if (rollbox.scrollTop >= ul1.scrollHeight) {
+        rollbox.scrollTop = 0;
+    } else {
+        rollbox.scrollTop++;
+    }
+}
+
+
 
 function rolls(t, ul1, ul2, rollbox) {
     ul2.innerHTML = ul1.innerHTML;
@@ -267,7 +504,9 @@ function rolls(t, ul1, ul2, rollbox) {
     rollbox.onmouseout = function () {
         timer = setInterval(rollStarts, t);
     }
-}function rolls1(t, ul1, ul2, rollbox) {
+}
+
+function rolls1(t, ul1, ul2, rollbox) {
     ul2.innerHTML = ul1.innerHTML;
     rollbox.scrollTop = 0;
     let timer = setInterval(rollStarts1, t);
@@ -287,7 +526,9 @@ function rollStarts() {
     } else {
         rollbox.scrollTop++;
     }
-}function rollStarts1() {
+}
+
+function rollStarts1() {
     let ul1 = document.getElementById("OnDutyToday1");
     let rollbox = document.getElementById("OnDutyToday_box1");
     if (rollbox.scrollTop >= ul1.scrollHeight) {
